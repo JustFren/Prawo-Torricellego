f93adb1c9175d6e3773d959e9d0806594d34b643
Adam <adam.fren@o2.pl>
2026-01-10 21:50:21 +0100

    ulepszony model, rozbicie dziedziny na mniejsze kawałki

--- metoda_eulera.py
+++ metoda_eulera.py
@@ -1,8 +1,9 @@
-import math
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 from scipy.interpolate import CubicSpline
+from approximations_of_a import approximated_a_10
+
 def plot(list):
     # make data
     #x = np.linspace(x0, x, int((x-x0)/epsilon))
@@ -43,13 +44,19 @@ def calc_error(list1,list2):
         out.append(list1[i]-list2[i])
     return out
 
-def compare_methods(x0,x,y0,epsilon,func):
+
+times=[0,64_800,151_200,241_200,327_600,414_000,583_200,669_600,756_000,842_400,928_800,1_008_000]
+volumes=[147,134,115,102,85,63,44,24,17,12,8,4]
+def compare_methods(x0,x,y0,epsilon,func,draw=1):
     euler=euler_method_array(x0,x,epsilon,y0,func)
     lsode=scipy.integrate.odeint(func,y0,np.linspace(x0,x,len(euler)),tfirst=True)
+    
+    if draw==0:
+        return [euler,lsode]
     err=calc_error(euler,lsode)
-    #mean_err=err
-    #mean_err.sort()
-    #Jmean_err=mean_err[len(mean_err)//2]
+    obj=[147,134,115,102,85,63,44,24,17,12,8,4]
+    for i in range(len(obj)):
+        obj[i]=volume_to_height(obj[i]*(10**6))
 
 
     x=np.linspace(x0,x,len(euler))
@@ -59,6 +66,8 @@ def compare_methods(x0,x,y0,epsilon,func):
     ax1.set_ylabel("Water level (m)")
     ax1.plot(x,euler,label="Euler")
     ax1.plot(x,lsode,label="ODEINT/LSODE")
+    
+    ax1.plot(times,obj,"^",label="Real data")
     ax1.legend()
     ax2.set_xlabel("Time (s)")
     ax2.set_ylabel("Error between Euler and LSODE (Euler-LSODE)")
@@ -68,17 +77,42 @@ def compare_methods(x0,x,y0,epsilon,func):
     #print(f"average error = {average_err}\nmean error = {mean_err}")
     plt.show()
     return [euler,lsode]
-#promień cylindra
-r=10
-#pole powierzchni przekroju cylindra
-A=math.pi*(r**2)
-#pole powierzchni otworu wylotowego na spodzie cylindra
-a=0.06*A
-#przyspieszenie grawitacyjne
-g=9.81
 
-def test_func(t,y):
-    return y
+def compare_methods_mult(times,volumes,epsilon,func):
+    volumes=[volume_to_height(volumes[i]*(10**6)) for i in range(len(volumes))]
+    euler=[]
+    lsode=[]
+    for i in range(len(times)-1):
+        euler.append(euler_method_array(times[i],times[i+1],epsilon,volumes[i],func))
+        lsode.append(scipy.integrate.odeint(func,volumes[i],np.linspace(times[i],times[i+1],len(euler)),tfirst=True))
+    
+    #err=calc_error(euler,lsode)
+    obj=[147,134,115,102,85,63,44,24,17,12,8,4]
+    for i in range(len(obj)):
+        obj[i]=volume_to_height(obj[i]*(10**6))
+
+
+    fig, ax1 = plt.subplots(nrows=1,ncols=1,sharex=True)
+    #ax.set_yscale("log")
+    ax1.set_xlabel("Time (s)")
+    ax1.set_ylabel("Water level (m)")
+    #ax2.set_xlabel("Time (s)")
+    #ax2.set_ylabel("Error between Euler and LSODE (Euler-LSODE)")
+    for i in range(len(times)-1):
+        x=np.linspace(times[i],times[i+1],len(euler[i]))
+        ax1.plot(x,euler[i],label="Euler",color="blue")
+        #ax1.plot(x,lsode[0][i],label="ODEINT/LSODE")
+    
+    ax1.plot(times,obj,"^",label="Real data",color="red")
+    #ax1.legend()
+
+    #ax2.plot(x,err)
+    
+
+
+    #print(f"average error = {average_err}\nmean error = {mean_err}")
+    plt.show()
+    #return [euler,lsode]
 
 #dla wody mi pomiędzy [0.60,0.65]
 mi=0.62
@@ -87,62 +121,38 @@ A0=26_300_000
 g=9.81
 h0=24.41
 Htotal=35.41
-#======
-#model nie jest do końca dokładny więc trzeba wprowadzić zmienną "terenową"
-#według tego modelu maksymalna pojemność to 208 mln m^3, ta zmienna to poprawia
-zm_ter=0.889
-#====
 def A(h):
     return A0 * (h/Htotal)**2
 Vwirt=1/3*A(h0)*h0
 
 def height_to_volume(h):
-    return ((A(h)*(h)/3)-Vwirt)*zm_ter
+    h=h+h0
+    return ((A(h)*(h)/3)-Vwirt)
 
 def volume_to_height(V):
-    return np.cbrt(3*(V+Vwirt)*(Htotal**2)/A0)
+    return np.cbrt(3*(V+Vwirt)*(Htotal**2)/A0)-h0
 
-print(volume_to_height(134_000_000))
-
-
-
-#========v
-times=[0,64_800,151_200,241_200,327_600,414_000,583_200,669_600,756_000,842_400,928_800,1_008_000]
 inflow=[1040,644.1,408.2,302.5,230.8,172.5,130.5,116.5,104.5,92.2,81.3,82.8]
-cs=CubicSpline(times,inflow)
-
-
-def Q_in(t,h):
-    def Q_calc(h,inflow):
-        return volume_to_height(inflow+height_to_volume(h))-volume_to_height(height_to_volume(h))
-    #xs=np.arange(0,1_000_000,1)
-    #plot(cs(xs))
-    return Q_calc(h,cs(t))
-#========^    
-
-wsp=(-1)*mi*a*(1/2)
+wsp=mi*a
 G=2*g
+Q_in=CubicSpline(times,inflow)
+
+
+apprx_a_func=CubicSpline(times,approximated_a_10)
 def approx_raciborz(t,h):
+    def Q_out(t,h):
+        return mi*a*apprx_a_func(t)*np.sqrt(G*h)
     if h<=0:
         return 0
-    return Q_in(t,h)+((wsp * np.sqrt(G*h))/A(h+h0))
+    return (Q_in(t)-Q_out(t,h))/A(h+h0)
+
+compare_methods(0,times[-1],volume_to_height(volumes[0]*(10**6)),1,approx_raciborz)  
+compare_methods_mult(times,volumes,1,approx_raciborz)
+
+#wynik=compare_methods(0,1_000_000,volume_to_height(147_000_000),1,approx_raciborz)
 
 testlist=[147,134,115,102,85,63,44,24,17,12,8,4]
 
-#for i in range(len(testlist)):
-#    print(volume_to_height(testlist[i]))
-
-
-
-
-wynik=compare_methods(0,64_800,volume_to_height(147_000_000),0.5,approx_raciborz)
-
-
-
-
-for i in range(len(testlist)):
-    testlist[i]=volume_to_height(testlist[i]*(10**6))
-plot(testlist)
 """
 * 17 września o 16:00 poziom wody wynosił ok. 147 mln m³,t=0s
 * 18 września o 10.00 poziom wody wynosił ok. 134 mln m³,t=64_800s
@@ -158,4 +168,10 @@ plot(testlist)
 * 28 września o 10:00 poziom wody wynosił ok. 4 mln m³,t=928_800
 * 29 września 2024 ok godziny 8 zakończyła się praca zbiornika Racibórz Dolny, poziom wody wynosi 0m³.t=1_008_000
 """
-#TODO zmienić objętości na wartości h
+
+def approx_raciborz_redundant(t,h):
+    def Q_out(h):
+        return min(1200,wsp*np.sqrt(G*h))  
+    if h<=0:
+        return 0
+    return (Q_in(t)-Q_out(h))/A(h+h0)
